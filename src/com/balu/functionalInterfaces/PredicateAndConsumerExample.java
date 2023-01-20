b68d07d59525519280c9925696f158fca21426b4
crassy <crassydeveloper@gmail.com>
2023-01-20 21:04:38 +0530

    Using Predicate and BiPredicate Interfaces added.

--- src/com/balu/functionalInterfaces/PredicateAndConsumerExample.java
+++ src/com/balu/functionalInterfaces/PredicateAndConsumerExample.java
@@ -5,6 +5,7 @@ import com.balu.data.StudentDatabase;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -18,13 +19,26 @@ public class PredicateAndConsumerExample {
         studentList.forEach(studentConsumer);
     }
 
-    Predicate<Student> studentPredicateGradeLevel = (student) -> student.getGradeLevel() >= 3;
-    Predicate<Student> studentPredicateGPA = (student) -> student.getGpa() >= 3.9;
+    //Using Predicate.
+    /*Predicate<Student> studentPredicateGradeLevel = (student) -> student.getGradeLevel() >= 3;
+    Predicate<Student> studentPredicateGPA = (student) -> student.getGpa() >= 3.9;*/
+
+    //Using BiPredicate.
+    BiPredicate<Integer, Double> biPredicate = (gradeLevel, gpa) -> gradeLevel >= 3 && gpa >= 3.9;
 
     BiConsumer<String, List<String>> stringListBiConsumer = (name, activities) -> System.out.println(name + " : " + activities);
 
     Consumer<Student> studentConsumer = (student) -> {
+        //Using Predicate.
+        /**
+         *
         if(studentPredicateGradeLevel.and(studentPredicateGPA).test(student)){
+            stringListBiConsumer.accept(student.getName(), student.getActivities());
+        }
+         */
+
+        //Using BiPredicate.
+        if(biPredicate.test(student.getGradeLevel(), student.getGpa())){
             stringListBiConsumer.accept(student.getName(), student.getActivities());
         }
     };
